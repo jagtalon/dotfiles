0f4ab886ee143f64ad1332860b2a3dc63ea14bf1
Jag Talon <jag@jagtalon.com>
2020-12-09 13:52:14 -0500

    Small update to tarsnap-backup.sh

--- tarsnap-backup.sh
+++ tarsnap-backup.sh
@@ -3,4 +3,4 @@
 set directories "/mnt/c/Users/jag/Documents" "/mnt/c/Users/jag/Desktop" "/home/jag"
 
 /usr/bin/tarsnap -v -c -f (uname -n)"-"(date +%Y-%m-%d_%H-%M-%S) \
-	$directories
+    $directories
