01b0333c2abcdf1ca10852041e3918898defa927
Jag Talon <jag@jagtalon.com>
2020-12-18 17:33:50 -0500

    tarsnap.fish: Add prune function

--- .config/fish/functions/tarsnap.fish
+++ .config/fish/functions/tarsnap.fish
@@ -1,14 +1,16 @@
 function tarsnap
     switch $argv[1]
-    case stats
-        command tarsnap --print-stats --humanize
-    case archives
-        command tarsnap --list-archives | sort | tail
-    case extract
-        command tarsnap -xf $argv[2..-1]
-    case ls
-        command tarsnap -tf $argv[2..-1]
-    case '*'
-        command tarsnap $argv
+        case stats
+            command tarsnap --print-stats --humanize
+        case archives
+            command tarsnap --list-archives | sort | tail
+        case extract
+            command tarsnap -xf $argv[2..-1]
+        case ls
+            command tarsnap -tf $argv[2..-1]
+        case prune
+            command tarsnap --list-archives | prunef --keep-hourly 4 --keep-daily 7 --keep-weekly 4 --keep-monthly 12 "Jag-%Y-%m-%d_%H-%M-%S" | xargs -n1 tarsnap -df
+        case '*'
+            command tarsnap $argv
     end
 end
