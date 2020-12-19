08def118d9d1c939d0fcbe0b37b7a8fbdf42ed0e
Jag Talon <jag@jagtalon.com>
2020-12-18 21:20:33 -0500

    Add prunef whenever backups are run.

--- .config/fish/functions/tarsnap.fish
+++ .config/fish/functions/tarsnap.fish
@@ -3,13 +3,13 @@ function tarsnap
         case stats
             command tarsnap --print-stats --humanize
         case archives
-            command tarsnap --list-archives | sort | tail
+            command tarsnap --list-archives | sort
         case extract
             command tarsnap -xf $argv[2..-1]
         case ls
             command tarsnap -tf $argv[2..-1]
         case prune
-            command tarsnap --list-archives | prunef --keep-hourly 4 --keep-daily 7 --keep-weekly 4 --keep-monthly 12 "Jag-%Y-%m-%d_%H-%M-%S" | xargs -n1 tarsnap -df
+            command tarsnap --list-archives | /usr/local/bin/prunef --keep-secondly 5 --keep-hourly 6 --keep-daily 7 --keep-weekly 4 --keep-monthly 12 "Jag-%Y-%m-%d_%H-%M-%S" | xargs --no-run-if-empty -n1 tarsnap -df
         case '*'
             command tarsnap $argv
     end
--- tarsnap-backup.sh
+++ tarsnap-backup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/fish
 
+# List directories to be backed up.
 set directories "/mnt/c/Users/jag/Documents" "/mnt/c/Users/jag/Desktop" "/home/jag"
 
+# Start backup.
 /usr/bin/tarsnap -v -c -f (uname -n)"-"(date +%Y-%m-%d_%H-%M-%S) \
     $directories
+
+# Prune unnecessary files.
+/usr/bin/tarsnap --list-archives | /usr/local/bin/prunef --keep-secondly 5 --keep-hourly 6 --keep-daily 7 --keep-weekly 4 --keep-monthly 12 "Jag-%Y-%m-%d_%H-%M-%S" | xargs --no-run-if-empty -n1 tarsnap -df
