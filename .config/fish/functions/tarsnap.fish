b46dc9c38c4daba909aac35aee493682919e1428
Jag Talon <jag@jagtalon.com>
2020-12-18 23:51:11 -0500

    tarsnap.fish: Only run `prunef` in backup script

--- .config/fish/functions/tarsnap.fish
+++ .config/fish/functions/tarsnap.fish
@@ -8,8 +8,6 @@ function tarsnap
             command tarsnap -xf $argv[2..-1]
         case ls
             command tarsnap -tf $argv[2..-1]
-        case prune
-            command tarsnap --list-archives | /usr/local/bin/prunef --keep-secondly 5 --keep-hourly 6 --keep-daily 7 --keep-weekly 4 --keep-monthly 12 "Jag-%Y-%m-%d_%H-%M-%S" | xargs --no-run-if-empty -n1 tarsnap -df
         case '*'
             command tarsnap $argv
     end
